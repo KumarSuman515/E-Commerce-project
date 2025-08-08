7aea93cdbdccfa1f4c096f528fcfeb42c09242de
kumarsuman55 <suman@arecle.net>
2025-08-08 16:13:16 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "unsafe-none", // Loosens COOP so Firebase popups can be checked
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
